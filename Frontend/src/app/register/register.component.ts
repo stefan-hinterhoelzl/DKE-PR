adc493432ace46f90d911f551cf812bcce8d99c0
hinterhoelzl-stefan <hinterhoelzlstefan@gmail.com>
2021-04-19 18:18:17 +0200

    small changes to registration form

--- Frontend/src/app/register/register.component.ts
+++ Frontend/src/app/register/register.component.ts
@@ -17,6 +17,7 @@ export class RegisterComponent implements OnInit {
   Colors: any = ['Blau', 'Rot', 'Gelb', 'Lila', 'Grün', 'Pink'];
 
   errorMessage: String;
+  successMessage: String;
   error: boolean;
   form: FormGroup;
   success: boolean;
@@ -58,13 +59,30 @@ export class RegisterComponent implements OnInit {
       var addedUser: User;
       this.apiservice.postUser(user).subscribe((data: User) => {
         addedUser = {...data}
-        this.router.navigate(['app']);
+        this.successMessage = "User "+addedUser.email+" wurde angelegt.";
+        this.success = true;
+        setTimeout(() => this.success = false, 3500);
+        
       },
       (error) => {
         this.errorMessage = error.error;
         this.error = true;
         setTimeout(() => this.error = false, 3500);
       });
+      
+      this.form.reset();
+
+      Object.keys(this.form.controls).forEach(key => {
+        this.form.get(key).setErrors(null);
+      });
+
+      // this.form.setValue({
+      //   email: "",
+      //   firstname: "",
+      //   lastname: "",
+      //   password: "",
+      //   phonenumber: "",
+      // });
     }
   }
 
