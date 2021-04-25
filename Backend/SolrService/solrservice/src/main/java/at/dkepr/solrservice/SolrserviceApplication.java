cba3311888d59b202a5a192cb447186ced25e1d9
hinterhoelzl-stefan <hinterhoelzlstefan@gmail.com>
2021-04-25 13:26:01 +0200

    solrservice added

---
+++ Backend/SolrService/solrservice/src/main/java/at/dkepr/solrservice/SolrserviceApplication.java
@@ -0,0 +1,13 @@
+package at.dkepr.solrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrserviceApplication.class, args);
+	}
+
+}
---
+++ Backend/SolrService/solrservice/src/test/java/at/dkepr/solrservice/SolrserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.dkepr.solrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
