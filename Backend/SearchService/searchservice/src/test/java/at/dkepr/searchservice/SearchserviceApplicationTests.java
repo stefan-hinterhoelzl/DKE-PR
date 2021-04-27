883cca27b05ba478dfb299e6e04e90f6367ca96f
hinterhoelzl-stefan <hinterhoelzlstefan@gmail.com>
2021-04-27 17:08:11 +0200

    searchservice added

---
+++ Backend/SearchService/searchservice/src/test/java/at/dkepr/searchservice/SearchserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.dkepr.searchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
