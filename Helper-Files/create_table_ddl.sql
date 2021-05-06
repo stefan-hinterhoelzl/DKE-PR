22473ee54431378cdc233f6bdf735fc2a9d11892
hinterhoelzl-stefan <hinterhoelzlstefan@gmail.com>
2021-05-07 00:04:02 +0200

    helper files

---
+++ Helper-Files/create_table_ddl.sql
@@ -0,0 +1,9 @@
+Create Table Users (
+	id number Primary Key,
+	email varchar2(200) Unique Not Null,
+	firstname varchar2(200) Not Null,
+	lastname varchar2(200) Not Null,
+	phonenumber varchar2(200),
+	password varchar2(500) Not Null,
+	pokemonid varchar2(20) Not Null
+);
