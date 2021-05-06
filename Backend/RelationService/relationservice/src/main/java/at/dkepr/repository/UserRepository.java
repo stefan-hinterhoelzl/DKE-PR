a0f9344d18a90009bea56694e7ab235b692215f4
stfbolda <stfbolda@gmail.com>
2021-05-07 00:05:41 +0200

    pom & config update

--- Backend/RelationService/relationservice/src/main/java/at/dkepr/repository/UserRepository.java
+++ Backend/RelationService/relationservice/src/main/java/at/dkepr/repository/UserRepository.java
@@ -5,11 +5,11 @@ import java.util.Collection;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import at.dkepr.model.User;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends Neo4jRepository<User, Long> {
     
     User findByEmail(@Param("email") String email);
